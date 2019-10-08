0fc0db2675f3dc5941d941445ac8e58fad9063a1
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-08 17:07:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # Your Code Here
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
+end
